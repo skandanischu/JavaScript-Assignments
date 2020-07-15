4adbfd7acdb4297c1365012d39dac84de36b0039
Nischay K A <56576590+skandanischu@users.noreply.github.com>
2020-07-16 00:43:39 +0530

    Delete Day 2 Assignment 1.js

--- Day 2 Assignment 1.js
+++
@@ -1,7 +0,0 @@
-console.log("hello")
-    // The Best Example for Console.log was given in today's class.
-
-let age = prompt("what is our age");
-console.log(age);
-
-// you can put anything in the place of the string like number, words, boolean values etc...
