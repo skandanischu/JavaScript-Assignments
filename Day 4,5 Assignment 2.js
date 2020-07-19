29d41bc6d8ad0784b0aec42b9b272b6650e7df1b
Nischay K A <56576590+skandanischu@users.noreply.github.com>
2020-07-19 23:12:49 +0530

    Day 4 Assignment 2

---
+++ Day 4,5 Assignment 2.js
@@ -0,0 +1,11 @@
+console.log("Destructure")
+    // lets destruct the program
+
+let person = {
+    name: "Helsinki",
+    age: 19,
+    projects: { DiceGame: "two players dicegame using JS" }
+}
+console.log(person.age);
+console.log(person.name);
+console.log(person.projects);
