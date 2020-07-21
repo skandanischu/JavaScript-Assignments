b1fdf78890dc56c8bd818cba7f1c52dc88e52697
Nischay K A <56576590+skandanischu@users.noreply.github.com>
2020-07-21 12:46:45 +0530

    Day 5 Assignment 2

---
+++ Day 5 Assignment 2.js
@@ -0,0 +1,12 @@
+const userOne = {
+    name: 'Skanda',
+    age: 20,
+    email: 'Skanda@gmail.com',
+    login() {
+        console.log('Skanda has logged in');
+    },
+    logout() {
+        console.log('Skanda has logged out');
+    },
+}
+console.log(userOne.email);
